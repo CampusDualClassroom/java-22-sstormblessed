cd2b78b26c0f43d963028499b15f2ba17dde4818
Nicolás Vázquez <nikovazquez52@gmail.com>
2024-09-26 14:47:27 +0200

    ejercicio completo

--- src/main/java/com/campusdual/classroom/Exercise22.java
+++ src/main/java/com/campusdual/classroom/Exercise22.java
@@ -7,15 +7,28 @@ import java.util.Set;
 public class Exercise22 {
 
     public static List<String> createArrayList() {
-
+        List<String> lista = new ArrayList<>();
+        lista.add("GKFFD");
+        lista.add("TNANA");
+        lista.add("MPMSL");
+        lista.add("PSWME");
+        lista.add("LZMLF");
+        lista.add("JYEBV");
+        lista.add("YELNT");
+        lista.add("JSNKR");
+        lista.add("JFESF");
+        lista.add("TMJLL");
+        return lista;
     }
 
     public static void printElementsAndIndex(List<String> customList) {
-
+        for (String elemento : customList) {
+            System.out.println(elemento);
+        }
     }
 
     public static boolean addElementToList(List<String> customList, String element) {
-
+        return customList.add(element);
     }
 
     public static void main(String[] args) {
